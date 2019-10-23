200e125a998897f9a41e3026ed1aae2689e7b11a
zmiles17 <zgm_striker17@hotmail.com>
2019-10-23 18:31:51 -0400

    Part 5 Tests Passing

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
-import { Album } from 'app/album';
+import { ProductService } from '../product.service';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-tracklisting',
